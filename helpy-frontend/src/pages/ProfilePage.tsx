5211d59283f261df898f8e432e63bc805c3f4565
Dominique DEBERT <dominiquedebert@gmail.com>
2025-04-23 15:59:28 +0200

    FIX/ Added full day of birth

--- helpy-frontend/src/pages/ProfilePage.tsx
+++ helpy-frontend/src/pages/ProfilePage.tsx
@@ -8,14 +8,6 @@ const ProfilePage = () => {
     return <div>Vous devez être connecté pour voir votre profil.</div>;
   }
 
-const dateNaissance = user?.birthDate
-  ? new Date(user.birthDate).toLocaleDateString("fr-FR", {
-      month: "long",
-      day: "2-digit",
-      year: "numeric"
-    })
-  : null;
-
   return (
     <div className="profile-container p-10 w-full h-100">
       {user ? (
@@ -23,7 +15,7 @@ const dateNaissance = user?.birthDate
           <h1 className="text-2xl font-semibold text-primary">{user.firstName} {user.lastName}</h1>
           <p>Bienvenue sur votre profil</p>
           <p>Email: {user.email}</p>
-          <p>Date de naissance: {dateNaissance ? formatDate(dateNaissance) : 'Non disponible'}</p>
+          <p>Date de naissance: {user.birthDate ? formatDate(user.birthDate) : 'Non disponible'}</p>
         </>
       ) : (
         <p>Les informations de profil sont introuvables.</p>
