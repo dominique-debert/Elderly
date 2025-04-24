fa917a636031194f8166653cabb1ffa5e44f5fe5
Dominique DEBERT <dominiquedebert@gmail.com>
2025-04-24 08:25:26 +0200

    Convert Navbar to component

--- helpy-frontend/src/components/Layout.tsx
+++ helpy-frontend/src/components/Layout.tsx
@@ -1,34 +1,15 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import { useAuthStore } from '../store/auth';
-import { mdiBellOutline, mdiCogOutline } from '@mdi/js';
-import Icon from '@mdi/react';
+import Navbar from './Navbar';
 
 const Layout = () => {
-  const { isAuthenticated, user } = useAuthStore();
+  const { isAuthenticated } = useAuthStore();
 
   return (
     <div className="no-scrollbar overflow-x-hidden overflow-y-hidden bg-100">
-      <div className="navbar bg-white shadow-lg">
-        <div className="flex justify-between w-full items-center">
-          <Link to="/" className="btn btn-ghost text-2xl text-primary hover:bg-primary/30 rounded-xl">
-            Helpy
-          </Link>
 
-          <div className='flex space-x-0'>
-            <Link to="/profile" className="btn btn-ghost text-gray-400 hover:bg-primary/30 rounded-lg">
-              <Icon path={mdiBellOutline} size={1} />
-            </Link>
-
-            {user?.isAdmin && (
-              <Link to="/profile" className="btn btn-ghost text-gray-400 hover:bg-primary/30 rounded-lg">
-                <Icon path={mdiCogOutline} size={1} />
-              </Link>
-            )}
-          </div>
-        </div>
-      </div>
-
+     <Navbar />
       <main className="min-h-[calc(100vh-64px)] flex flex-direction-column">
         {isAuthenticated && <Sidebar />}
         <Outlet />
---
+++ helpy-frontend/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { mdiBellOutline, mdiCogOutline } from '@mdi/js';
+import Icon from '@mdi/react';
+import { useAuthStore } from '../store/auth';
+
+const Navbar = () => {
+  const { user } = useAuthStore();
+
+  return (
+    <div className="navbar bg-white shadow-lg">
+      <div className="flex justify-between w-full items-center">
+        <Link to="/" className="btn btn-ghost text-2xl text-primary hover:bg-primary/30 rounded-xl">
+          Helpy
+        </Link>
+
+        <div className='flex space-x-0'>
+          <Link to="/profile" className="btn btn-ghost text-gray-400 hover:bg-primary/30 rounded-lg">
+            <Icon path={mdiBellOutline} size={1} />
+          </Link>
+
+          {user?.isAdmin && (
+            <Link to="/profile" className="btn btn-ghost text-gray-400 hover:bg-primary/30 rounded-lg">
+              <Icon path={mdiCogOutline} size={1} />
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
