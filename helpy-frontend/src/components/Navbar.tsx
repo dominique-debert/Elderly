1b08cd3f9db5aa88746e36f1e7d674128c123ecf
Dominique DEBERT <dominiquedebert@gmail.com>
2025-04-24 21:23:21 +0200

    FEAT/ Improve navbar

--- helpy-frontend/src/components/Navbar.tsx
+++ helpy-frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const { user } = useAuthStore();
 
   return (
-    <div className="navbar bg-white shadow-lg">
+    <div className="navbar bg-white border border-b-gray-200 border-r-0 border-l-0 border-t-0">
       <div className="flex justify-between w-full items-center">
         <Link to="/" className="btn btn-ghost text-2xl text-primary hover:bg-primary/30 rounded-xl">
           Helpy
