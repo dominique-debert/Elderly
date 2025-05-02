8ed0b573d89b01362fe0c532a72eac399f19688c
Dominique DEBERT <dominiquedebert@gmail.com>
2025-05-02 18:08:09 +0200

    FEAT: Home dashboard: responsive

--- helpy-frontend/src/components/MedicationCard.tsx
+++ helpy-frontend/src/components/MedicationCard.tsx
@@ -1,107 +1,64 @@
+import Icon from '@mdi/react';
+import { mdiClockOutline } from '@mdi/js';
+
 const MedicationCard: React.FC = () => {
 
   return (
     <div className="card lg:w-full bg-base-100 border border-gray-200">
       <div className="w-full max-w-xl mx-auto p-4">
-        <h2 className="text-2xl font-bold mb-6 text-center text-primary">Prise de médicaments</h2>
+        <h2 className="text-2xl font-semibold mb-6 text-center text-primary">Prise de médicaments</h2>
         <ul className="timeline timeline-vertical lg:timeline-horizontal">
           <li>
-            <div className="timeline-start">1984</div>
+            <div className="timeline-start">Matin</div>
             <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <Icon path={mdiClockOutline}
+                title="Parler"
+                size={1}
+                className="text-primary"
+              />
             </div>
-            <div className="timeline-end timeline-box">First Macintosh computer</div>
+            <div className="timeline-end timeline-box">Médicament 1</div>
             <hr />
           </li>
           <li>
             <hr />
-            <div className="timeline-start">1998</div>
+            <div className="timeline-start">Midi</div>
             <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <Icon path={mdiClockOutline}
+                title="Parler"
+                size={1}
+                className="text-primary"
+              />
             </div>
-            <div className="timeline-end timeline-box">iMac</div>
+            <div className="timeline-end timeline-box">Médicament 2</div>
             <hr />
           </li>
           <li>
             <hr />
-            <div className="timeline-start">2001</div>
+            <div className="timeline-start">Soir</div>
             <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+            <div className="timeline-middle">
+              <Icon path={mdiClockOutline}
+                title="Parler"
+                size={1}
+                className="text-primary"
+              />
             </div>
-            <div className="timeline-end timeline-box">iPod</div>
+            </div>
+            <div className="timeline-end timeline-box">Médicament 3</div>
             <hr />
           </li>
           <li>
             <hr />
-            <div className="timeline-start">2007</div>
+            <div className="timeline-start">Nuit</div>
             <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <Icon path={mdiClockOutline}
+                title="Parler"
+                size={1}
+                className="text-primary"
+              />
             </div>
-            <div className="timeline-end timeline-box">iPhone</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-start">2015</div>
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end timeline-box">Apple Watch</div>
+            <div className="timeline-end timeline-box">Médicament 4</div>
           </li>
         </ul>        
       </div>
