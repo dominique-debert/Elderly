4ea07d906439c77d0164e5374172670d78e5265d
Dominique DEBERT <dominiquedebert@gmail.com>
2025-05-05 19:19:10 +0200

    FEAT/ Add mood pills

--- helpy-frontend/src/components/GeneralMetricsCard.tsx
+++ helpy-frontend/src/components/GeneralMetricsCard.tsx
@@ -12,10 +12,21 @@ return (
     >
       <div className="flex flex-col w-full h-full">
         <h2 className="card-title mt-4 mb-4">Mesures générales</h2>
-
-        <div className="grid grid-cols-3 grid-rows-2 gap-4 h-full">
+        <div className="grid grid-cols-4 gap-4 h-full">
+        <div className="card flex justify-end items-center align-middle bg-pink-50 row-span-2 col-span-2 ml-1">
+          <span className="m-4 absolute top-2 left-2">Humeur</span>
+          <div className="badge bg-green-600 m-2 p-4 rotate-12 text-white lg:text-base">concentré</div>
+          <span>
+            <div className="badge bg-orange-400 text-white mt-4 p-4 -rotate-45 lg:text-base">heureux</div>
+            <div className="badge bg-red-500 text-white ml-[-15px] p-4 -rotate-12 lg:text-base">en colère</div>
+            <div className="badge bg-pink-500 text-white ml-[-15px] p-4 rotate-90 lg:text-base">inspiré</div>
+          </span>
+          <span>
+            <div className="badge bg-violet-400 text-white mt-6 p-4 lg:text-base">détendu</div>
+            <div className="badge bg-cyan-400 text-white mt-6 p-4 rotate-45 lg:text-base mb-6">triste</div>
+          </span>
+        </div>
           {[
-            { id: 1, bg: "bg-pink-50", className: "row-span-2" },
             { id: 2, bg: "bg-indigo-50" },
             { id: 3, bg: "bg-yellow-50" },
             { id: 4, bg: "bg-blue-50", className: "col-span-2" },
