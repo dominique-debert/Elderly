be3b78f13490c392149f8b0f5c26761fc2825497
Dominique DEBERT <dominiquedebert@gmail.com>
2025-05-08 17:34:07 +0200

    FEAT/ Autosized mood cards

--- helpy-frontend/src/components/MoodList.tsx
+++ helpy-frontend/src/components/MoodList.tsx
@@ -13,7 +13,7 @@ export default function MoodList() {
   if (isError) return <div className="text-center mt-10 text-red-500">Erreur de chargement</div>;
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 h-full w-full">
+    <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
       {moods?.map((mood: IMood) => (
         <MoodCard key={mood.id} mood={mood} />
       ))}
