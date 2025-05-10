fc2fbd22382a739bc0c90e4a4fdb9b1c3d6f2cfe
Dominique DEBERT <dominiquedebert@gmail.com>
2025-05-10 06:24:58 +0200

    FEAT/ Add seeding file for notifications

--- api-server/prisma/edge.js
+++ api-server/prisma/edge.js
@@ -798,11 +798,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:1272@services:32768/helpy?schema=public"
+        "value": null
       }
     }
   },
--- api-server/prisma/index.js
+++ api-server/prisma/index.js
@@ -799,11 +799,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:1272@services:32768/helpy?schema=public"
+        "value": null
       }
     }
   },
