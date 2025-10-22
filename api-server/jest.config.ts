ca9dba1bbf8e7b7b0dde5c8eea5e588f5e25ee1e
Dominique DEBERT <dominiquedebert@gmail.com>
2025-10-22 19:24:21 +0200

    chore/move tests to __tests__

--- api-server/jest.config.ts
+++ api-server/jest.config.ts
@@ -3,32 +3,23 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  testMatch: ["**/*.test.ts"],
+  roots: ["<rootDir>"],
+  testMatch: ["**/__tests__/**/*.test.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
   collectCoverageFrom: [
     "controllers/**/*.ts",
-    "!controllers/**/*.test.ts",
+    "routes/**/*.ts",
+    "middlewares/**/*.ts",
+    "utils/**/*.ts",
+    "!**/*.d.ts",
     "!**/node_modules/**",
+    "!**/__tests__/**",
   ],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
-  transform: {
-    "^.+\\.ts$": [
-      "ts-jest",
-      {
-        tsconfig: {
-          module: "esnext",
-          target: "esnext",
-          allowSyntheticDefaultImports: true,
-          esModuleInterop: true,
-        },
-      },
-    ],
-  },
+  verbose: true,
 };
 
 export default config;
