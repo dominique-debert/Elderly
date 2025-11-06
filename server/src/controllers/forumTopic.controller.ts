91a0780c2d26346c6b98beb61a6b62b675b1e555
Dominique DEBERT <dominiquedebert@gmail.com>
2025-11-06 09:35:41 +0100

    feat/begin forum dev

---
+++ server/src/controllers/forumTopic.controller.ts
@@ -0,0 +1,135 @@
+import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@/prisma";
+import createHttpError from "http-errors";
+import { IForumTopic } from "@/types";
+
+const prisma = new PrismaClient();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Forum messages
+ *   description: API pour gérer les messages du forum
+ */
+
+export const createForumMessage = async (
+  req: Request<{}, {}, IForumTopic>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const forumTopicToCreate = await prisma.forumTopic.create({
+      data: {
+        title: req.body.title,
+        pinned: req.body.pinned,
+        status: req.body.status,
+        views: req.body.views,
+        user: {
+          connect: { id: req.body.author_id },
+        },
+        category: {
+          connect: { id: req.body.category_id },
+        },
+      },
+    });
+    res.status(201).json(forumTopicToCreate);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllForumTopics = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const forumTopics = await prisma.forumTopic.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    res.status(200).json({ forumTopics });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getForumTopicById = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const forumTopic = await prisma.forumTopic.findUnique({
+      where: { id },
+    });
+
+    if (!forumTopic) {
+      throw createHttpError(404, "Message non trouvé");
+    }
+
+    res.status(200).json(forumTopic);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateForumTopic = async (
+  req: Request<{ id: string }, IForumTopic>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const forumTopic = await prisma.forumTopic.findUnique({
+      where: { id },
+    });
+
+    if (!forumTopic) {
+      throw createHttpError(404, "Message non trouvé");
+    }
+
+    const forumTopicToUpdate = await prisma.forumTopic.update({
+      data: {
+        ...req.body,
+        updatedAt: new Date(),
+      },
+      where: { id },
+    });
+
+    res.status(200).json(forumTopicToUpdate);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteForumTopic = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const forumTopic = await prisma.forumTopic.findUnique({
+      where: { id },
+    });
+
+    if (!forumTopic) {
+      throw createHttpError(404, "Message non trouvé");
+    }
+
+    await prisma.forumTopic.delete({
+      where: { id },
+    });
+
+    res.status(200).json({ message: "Message supprimé avec succès" });
+  } catch (error) {
+    next(error);
+  }
+};
