bd972451010c0c6905d4d9982086cf3f29268dc2
Dominique DEBERT <dominiquedebert@gmail.com>
2025-11-20 16:41:56 +0100

    feat/Navbar: responsive

--- client/src/components/Layout/Navbar.tsx
+++ client/src/components/Layout/Navbar.tsx
@@ -69,69 +69,118 @@ export function Navbar() {
 
   return (
     <header className="header-area">
-      <div className="navbar bg-white dark:bg-card fixed top-0 z-50 h-16 w-full shadow-sm border-b border-base-200">
-        <div className="flex justify-between w-full items-center">
-          <Link to="/dashboard">
-            <div className="flex p-2 pl-3 items-center gap-4 font-normal text-2xl text-primary">
-              <img src="/images/elderly.webp" alt="Logo" className="h-8" />
-              <span className="text-primary text-2xl">Elderly</span>
-            </div>
-          </Link>
-
-          <Search
-            search={""}
-            placeholder={"Rechercher..."}
-            setSearch={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-
-          <div className="flex items-center gap-6">
-            {/* Theme Toggle */}
-
-            <label className="swap swap-rotate">
-              <input
-                type="checkbox"
-                className="theme-controller appearance-none"
-                value="nord"
-                onChange={handleToggle}
-                checked={theme === "nord"}
-              />
-              {theme === "nord" ? (
-                <SunIcon className="size-6 text-yellow-400 dark:text-white/50" />
-              ) : (
-                <MoonIcon className="size-6 text-slate-500 dark:text-white/40" />
-              )}
-            </label>
-
-            <div className="relative mr-4" ref={notifRef}>
-              <button
-                role="button"
-                className="cursor-pointer text-base-content avatar tooltip tooltip-bottom tooltip-accent"
-                data-tip="Notifications"
-                onClick={() => setIsNotifOpen(!isNotifOpen)}
+      <div className="drawer">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <div className="navbar md:block bg-white dark:bg-card fixed top-0 z-50 h-16 w-full shadow-sm border-b border-base-200">
+            <div className="flex-none md:hidden">
+              <label
+                htmlFor="my-drawer-2"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
               >
-                <BellIcon className="size-6 text-slate-500 dark:text-white/40" />
-
-                {notifications && notifications.length > 0 && (
-                  <span className="absolute p-1.5 avatar bg-red-600 rounded-xl right-0 bottom-0" />
-                )}
-              </button>
-              {isNotifOpen && (
-                <div
-                  className="absolute right-0 mt-2"
-                  onClick={(e) => e.stopPropagation()}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current"
                 >
-                  <NotificationList
-                    notifications={
-                      Array.isArray(notifications) ? notifications : []
-                    }
-                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+
+            <div className="md:visible">
+              <div className="flex justify-between w-full items-center">
+                <Link to="/dashboard">
+                  <div className="flex p-2 pl-3 items-center gap-4 font-normal text-2xl text-primary">
+                    <img
+                      src="/images/elderly.webp"
+                      alt="Logo"
+                      className="h-8"
+                    />
+                    <span className="text-primary text-2xl">Elderly</span>
+                  </div>
+                </Link>
+
+                <Search
+                  search={""}
+                  placeholder={"Rechercher..."}
+                  setSearch={function (): void {
+                    throw new Error("Function not implemented.");
+                  }}
+                />
+
+                <div className="flex items-center gap-6">
+                  {/* Theme Toggle */}
+
+                  <label className="swap swap-rotate">
+                    <input
+                      type="checkbox"
+                      className="theme-controller appearance-none"
+                      value="nord"
+                      onChange={handleToggle}
+                      checked={theme === "nord"}
+                    />
+                    {theme === "nord" ? (
+                      <SunIcon className="size-6 text-yellow-400" />
+                    ) : (
+                      <MoonIcon className="size-6 text-accent" />
+                    )}
+                  </label>
+
+                  <div className="relative mr-4" ref={notifRef}>
+                    <button
+                      role="button"
+                      className="cursor-pointer text-base-content avatar tooltip tooltip-bottom tooltip-accent"
+                      data-tip="Notifications"
+                      onClick={() => setIsNotifOpen(!isNotifOpen)}
+                    >
+                      <BellIcon className="size-6 text-slate-500 dark:text-white/40" />
+
+                      {notifications && notifications.length > 0 && (
+                        <span className="absolute p-1.5 avatar bg-red-600 rounded-xl right-0 bottom-0" />
+                      )}
+                    </button>
+                    {isNotifOpen && (
+                      <div
+                        className="absolute right-0 mt-2"
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        <NotificationList
+                          notifications={
+                            Array.isArray(notifications) ? notifications : []
+                          }
+                        />
+                      </div>
+                    )}
+                  </div>
                 </div>
-              )}
+              </div>
             </div>
           </div>
         </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>{" "}
       </div>
     </header>
   );
