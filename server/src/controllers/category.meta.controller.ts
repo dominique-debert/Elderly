715d474c19abaa0af4908f63c53ca6f7cfcf59bc
Dominique DEBERT <dominiquedebert@gmail.com>
2025-11-22 06:59:43 +0100

    chore/Prisma: reverted to prisma v6 due to incompatibility

--- server/src/controllers/category.meta.controller.ts
+++ server/src/controllers/category.meta.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { PrismaClient } from "@/prisma/client";
+import { PrismaClient } from "@/prisma";
 
 const prisma = new PrismaClient();
 
